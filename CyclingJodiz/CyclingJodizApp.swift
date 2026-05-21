3f0b5ce270e1e25db192293f1f3ee43361b22851
Nielklt <otnielkalit25@gmail.com>
2026-05-21 10:03:34 +0700

    Initial Commit

---
+++ CyclingJodiz/CyclingJodizApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyclingJodizApp.swift
+//  CyclingJodiz
+//
+//  Created by otnielkalit on 21/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CyclingJodizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
